e94ea33e4a19f482d64222238590eaf28d3aa612
Danae-DJ <danae_taiyaki@hotmail.com>
2025-01-11 12:07:32 -0600

    added name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
+    
+    
     }
 }
+//original video: https://video.byui.edu/media/t/1_1bulammf
+//the solution master: https://github.com/byui-cse/cse210-ww-student-sample-solutions/blob/main/week01/Exercise1/Program.cs 
+
