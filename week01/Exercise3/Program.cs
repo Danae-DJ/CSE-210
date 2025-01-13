41e903d47fcd9c36ced40eba868a63f976e01484
Danae-DJ <danae_taiyaki@hotmail.com>
2025-01-12 19:15:57 -0600

    added basic loop

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.WriteLine("What is the magic number? ");
+        string response = Console.ReadLine();
+        int magicNumber = int.Parse(response);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+            {
+                Console.WriteLine("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else
+                {
+                Console.WriteLine("You guesed it!");
+                }
+            }
     }
 }
