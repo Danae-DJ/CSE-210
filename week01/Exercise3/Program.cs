0d8800a3a2aadc01a3d72bc889490cd9801ee8f4
Danae-DJ <danae_taiyaki@hotmail.com>
2025-01-12 20:11:58 -0600

    added extra

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,18 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
-
-        Console.WriteLine("What is the magic number? ");
+        Console.WriteLine("Welcome to the Guess My Number Game!");
+        /*Console.WriteLine("What is the magic number? ");
         string response = Console.ReadLine();
         int magicNumber = int.Parse(response);
 
-        int guess = -1;
+        int guess = -1;*/
+        string playAgain =""; //extra
+        do
+        {
+        //ramdon
 
-        while (guess != magicNumber)
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101); 
+            int guess = -1;
+            int attempts = 0; //extra
+
+            Console.WriteLine("I have picked a magic number between 1 and 100. Try to guess it!");
+
+            while (guess != magicNumber)
             {
                 Console.WriteLine("What is your guess? ");
-                string guessInput = Console.ReadLine();
-                guess = int.Parse(guessInput);
+                guess = int.Parse(Console.ReadLine());
+
+                attempts++;
 
                 if (guess < magicNumber)
                 {
@@ -30,8 +42,18 @@ class Program
 
                 else
                 {
-                Console.WriteLine("You guesed it!");
+                        Console.WriteLine($"You guessed it! The magic number was {magicNumber}.");
+                        Console.WriteLine($"It took you {attempts} attempts.");
                 }
+
             }
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        
+        
+        }while (playAgain == "yes");
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
+
+//solution: https://github.com/byui-cse/cse210-ww-student-sample-solutions/blob/main/week01/Exercise3/Program.cs
