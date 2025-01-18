789c0af293d5446f429a65c156241fb10bde9fbb
Danae-DJ <danae_taiyaki@hotmail.com>
2025-01-18 16:51:41 -0600

    added random  generator phrases

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,10 +1,22 @@
-//PromptGenerator.cs:
+//step5: PromptGenerator.cs: the cuestions to the journal
+using System;
 public class PrompGenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person you met today?",
+        "What was the best part of your day?",
+        "What was the strongest emotion you felt today?",
+        "How did you see the hand of the Lord in your life today?",
+        "What is one thing you learned today?",
+        "What is the phrase of the day?, Why?",
+        
+    };
 
     public string GetRandomPrompt()
     {
-        return "";
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
     }
 }
