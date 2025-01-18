6048126dda75175cfd75064d29be050f21ad11c7
Danae-DJ <danae_taiyaki@hotmail.com>
2025-01-18 16:41:25 -0600

    added operations journal

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,4 +1,4 @@
-//step4: Journal.cs: operations to entrys
+//step4: Journal.cs: operations to entrys in to file
 using System.IO; //for file operations...
 public class Journal
 {
@@ -11,16 +11,36 @@ public class Journal
 
     public void Display()
     {
-        
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
 
-    public void SavToFile()
+    public void SavToFile(string fileName)
     {
-
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date} | {entry._promptText} | {entry._entryText}");
+            }
+        }
     }
 
-    public void LoadFromFile()
+    public void LoadFromFile( string fileName)
     {
-
+        string[] lines = File.ReadAllLines(fileName);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+            Entry entry = new Entry
+            {
+                _date = parts[0],
+                _promptText = parts[1],
+                _entryText = parts[2]
+            };
+            _entries.Add(entry);
+        }
     }
 }
