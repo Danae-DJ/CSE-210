499ddd73ea2582b60a8d2ce2479a73b851040a97
Danae-DJ <danae_taiyaki@hotmail.com>
2025-01-22 00:08:30 -0600

    added methods

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -7,6 +7,22 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
     
-        
+        Fractions f1 = new Fractions();
+        Console.WriteLine(f1.GetFractionsString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fractions f2 = new Fractions(5);
+        Console.WriteLine(f2.GetFractionsString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fractions f3 = new Fractions(3, 4);
+        Console.WriteLine(f3.GetFractionsString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fractions f4 = new Fractions(1, 3);
+        Console.WriteLine(f4.GetFractionsString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
+
+    
 }
