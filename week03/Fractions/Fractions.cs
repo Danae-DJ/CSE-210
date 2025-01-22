b0870f4d0d9943ff25c0e94892f806f407c68b48
Danae-DJ <danae_taiyaki@hotmail.com>
2025-01-21 23:11:34 -0600

    added fraction class

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Fractions
+{
+    private int _top;
+    private int _bottom;
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+    
+        
     }
 }
