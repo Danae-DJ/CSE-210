2e09cac0375eef1f8f67ad4eadc677761b0293e0
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-05 22:49:48 -0600

    added math

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+    
+        Assignment assignment = new Assignment("Josh Peck", "Analisys and Filosofy: Letter to Gnomo");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine("\n");
+
+        MathAssignment mathAssignment = new MathAssignment("Scarlet John", "Fractions", "Section 5.1", "1-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine("\n");
     }
 }
