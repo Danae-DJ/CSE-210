5ddb56541a0d5382f9482d4876f01b9f04bbbad5
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-08 16:09:18 -0600

    activity

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Threading;
 class Activity
 {
     private string _name;
@@ -10,26 +10,61 @@ class Activity
     {
         _name = name;
         _description = description;
-        _duration =duration;
+        _duration = duration;
     }
 
     public void DisplayStartMessage()
     {
+        Console.Clear();
+        Console.WriteLine($"Let's start with: {_name}");
+        Console.WriteLine(_description);
+        Console.WriteLine("Enter the duration (seconds): ");
+        _duration = int.Parse(Console.ReadLine());
 
+        Console.WriteLine("\nGo, get ready");
+        ShowSpinner(3);
     }
 
     public void DisplayEndingMessage()
     {
-
+        Console.WriteLine("");
+        Console.WriteLine($"Great Job! You have complete the activity.");
+        Console.WriteLine($"You spent {_duration} seconds on {_name}.");
+        ShowSpinner(3);
     }
 
     public void ShowSpinner(int seconds)
     {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write("-");
+            Thread.Sleep(250);
+            Console.Write("\b\b");
 
+            Console.Write("\\");
+            Thread.Sleep(250);
+            Console.Write("\b\b");
+
+            Console.Write("|");
+            Thread.Sleep(250);
+            Console.Write("\b\b");
+
+            Console.Write("/");
+            Thread.Sleep(250);
+            Console.Write("\b\b");
+        }
     }
 
     public void ShowCoundDown( int seconds)
     {
-        
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write("i");
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+        }
     }
 }
+
+//tutorial thread: https://www.codeproject.com/Tips/5305519/How-to-Action-a-Spinner-in-a-Console-Application
+//https://stackoverflow.com/questions/5866190/thread-sleep-c-net
