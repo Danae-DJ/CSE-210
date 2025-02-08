9f5617d992e6bfd076a8b845888d6c941be3969a
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-08 15:22:02 -0600

    desing

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+    
+        //create a menu
+        //create option
+        //create base
+        //crate activities
+        //choice
+        //pausing activity
+        //end activity
+        //continue other round or other activite
+        //extending
+    
+    
+    
+    
     }
 }
