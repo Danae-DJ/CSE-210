fa4a76889b96a703b8b950072ddddf75feba7b96
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-11 23:30:34 -0600

    added square

--- week06/Shapes/Square.cs
+++ week06/Shapes/Square.cs
@@ -1,16 +1,16 @@
 using System;
 using System.Reflection.Metadata.Ecma335;
 
-class Square
+public class Square: Shape
 {
     private double _side;
 
-    public Square( double side)
+    public Square(string color, double side): base(color)
     {
         _side = side;
     }
 
-    public double GetArea()
+    public override double GetArea()
     {
         return _side * _side;
     }
