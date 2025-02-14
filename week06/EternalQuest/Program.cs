d35db5cf1c6b5b7de374338be64f6a97a06b4ec0
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-13 23:20:34 -0600

    new menu program

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the EternalQuest Project.");
+    
+        GoalManager goalManager = new GoalManager();
+
+        while(true)
+        {
+            Console.WriteLine("\nWhat would you like to do?");
+            Console.WriteLine("1. Create a Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Record an Event");
+            Console.WriteLine("4. Save Goals");
+            Console.WriteLine("5. Load Goals");
+            Console.WriteLine("6. Exit");
+            Console.Write("Choose an option (1-6): ");
+
+            string choice = Console.ReadLine();
+
+            switch(choice)
+            {
+                case "1":
+                    goalManager.CreateGoal();
+                    break;
+
+                case "2":
+                    goalManager.ListGoals();
+                    break;
+
+                case "3":
+                    goalManager.RecordEvent();
+                    break;
+
+                case "4":
+                    goalManager.SaveGoals();
+                    break;
+
+                case "5":
+                    goalManager.LoadGoals();
+
+                case "6":
+                    Console.WriteLine("Goodbye Champion!");
+
+                default:
+                    Console.WriteLine("Invalid option. Try with other option.");
+                    break;
+            }
+
+        }
+    
     }
 }
