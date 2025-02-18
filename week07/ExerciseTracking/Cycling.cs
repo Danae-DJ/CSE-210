f8e9a565d2cf793b5d333e9bb9996882fe4140e3
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-18 17:25:38 -0600

    added cycling

--- week07/ExerciseTracking/Cycling.cs
+++ week07/ExerciseTracking/Cycling.cs
@@ -1,1 +1,27 @@
 //Cycling.cs: derived
+using System;
+public class Cycling: Activity
+{
+    private double _speed;
+
+    public Cycling(string date, int duration, double speed)
+    : base (date, duration)
+    {
+        _speed = speed;
+    }
+// modify formules...
+    public override double GetDistance()
+    {
+       return (_speed * DurationInMinutes) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return (60 / _speed);
+    }
+}
