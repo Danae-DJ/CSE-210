90dc0dad77c7ae79638cf166853479aae4a9e02a
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-18 17:11:28 -0600

    running

--- week07/ExerciseTracking/Running.cs
+++ week07/ExerciseTracking/Running.cs
@@ -4,8 +4,33 @@ public class Running: Activity
 {
     private double _distance;
     public Running(string date, int duration, double distance)
-    : base
+    : base(date, duration)
     {
-        
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+       return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (_distance / DurationInMinutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return (DurationInMinutes / _distance);
     }
 }
+
+
+/*The formula for Speed is given as 
+[Speed = Distance ÷ Time]. 
+To calculate the distance, 
+the speed formula can be molded 
+as [Distance = Speed × Time].*/
+
+//https://www.w3resource.com/csharp-exercises/data-types/csharp-data-type-exercise-7.php
+//https://stackoverflow.com/questions/36315622/using-a-mathematical-formula-to-define-player-speed 
