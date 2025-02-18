698419a50ed633a4d92e1d60375f6a77347a951f
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-18 16:55:17 -0600

    acivity base

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -7,6 +7,21 @@ public abstract class Activity
     private int _durationInMinutes;
     public Activity(string date, int duration)
     {
-        
+        _date = date;
+        _durationInMinutes = duration;
+    }
+
+    public string Date => _date;
+    public int DurationInMinutes => _durationInMinutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();//?
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{Date} {GetType().Name} ({DurationInMinutes} min): Distance {GetDistance():F2} km, Speed {GetSpeed():F2} kph, Pace {GetPace():F2} min/km";
     }
 }
+//formule
+//:F2 (decimals)
