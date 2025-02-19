6ff5e084327ef10e16989067e35eb189851ddf3e
Danae-DJ <danae_taiyaki@hotmail.com>
2025-02-18 18:01:00 -0600

    siwimmng

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -1,1 +1,28 @@
 //Swimming.cs: derived.cs
+using System;
+
+public class Swimming: Activity
+{
+    private int _lap;
+    
+    public Swimming(string date, int duration, int laps)
+    : base(date, duration)
+    {
+        _lap = laps;
+    }
+
+    public override double GetDistance()
+    {
+       return (_lap * 50) / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / DurationInMinutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return (DurationInMinutes / GetDistance());
+    }
+}
